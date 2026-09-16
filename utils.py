2f5b2c91e9a094332248c4334a418adbd9167d35
wassannc <balaram@wassan.org>
2026-09-16 07:14:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,47 +1,51 @@
 import streamlit as st
 import requests
-import gspread
-from google.oauth2.service_account import Credentials
 import pandas as pd
 
+
+# -------- MANYAM GOOGLE SHEET --------
+
+GOOGLE_SHEET_ID = "1HlLJW9CrWkKZ6xN4zd9T7IYlLaWclJTofyv_DRQXebA"
+
+
+@st.cache_data(ttl=300)
 def load_manyam_google_sheets():
 
-    SCOPES = [
-        "https://www.googleapis.com/auth/spreadsheets.readonly",
-        "https://www.googleapis.com/auth/drive.readonly"
-    ]
-
-    creds = Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=SCOPES
+    # Total list
+    total_url = (
+        f"https://docs.google.com/spreadsheets/d/"
+        f"{GOOGLE_SHEET_ID}/gviz/tq?tqx=out:csv&gid=0"
     )
 
-    client = gspread.authorize(creds)
-
-    spreadsheet = client.open_by_key(
-        "1HlLJW9CrWkKZ6xN4zd9T7IYlLaWclJTofyv_DRQXebA"
+    # Working HHs
+    working_url = (
+        f"https://docs.google.com/spreadsheets/d/"
+        f"{GOOGLE_SHEET_ID}/gviz/tq?tqx=out:csv&gid=836274918"
     )
 
-    total_list = pd.DataFrame(
-        spreadsheet.worksheet("Total list").get_all_records()
-    )
-
-    working_hhs = pd.DataFrame(
-        spreadsheet.worksheet("Working HHs").get_all_records()
-    )
+    total_list = pd.read_csv(total_url)
+    working_hhs = pd.read_csv(working_url)
 
     return total_list, working_hhs
+
+
+# -------- ODK --------
 
 ODK_URL = st.secrets["ODK_URL"]
 USERNAME = st.secrets["USERNAME"]
 PASSWORD = st.secrets["PASSWORD"]
 PROJECT_ID = st.secrets["PROJECT_ID"]
 
+
 @st.cache_data(ttl=300)
 def load_odk_data(form_id):
+
     url = f"{ODK_URL}/v1/projects/{PROJECT_ID}/forms/{form_id}.svc/Submissions"
-    
-    response = requests.get(url, auth=(USERNAME, PASSWORD))
+
+    response = requests.get(
+        url,
+        auth=(USERNAME, PASSWORD)
+    )
 
     if response.status_code != 200:
         st.error(f"Error: {response.status_code}")
@@ -53,4 +57,5 @@ def load_odk_data(form_id):
         return pd.DataFrame()
 
     df = pd.json_normalize(data["value"])
+
     return df
