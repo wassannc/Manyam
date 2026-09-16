e0dc04e614ae63d8299ec183dd46e90653bf674a
wassannc <balaram@wassan.org>
2026-09-16 10:59:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 FORMS = {
     "1. NF- Register": {
         "form_id": "NF- Register",
-        "columns": ["plot_reg.date","plot_reg.block","plot_reg.gp","plot_reg.village","plot_reg.farmer_name","plot_reg.spouse","plot_reg.season","Crop Model Final","plot_reg.main_crop","plot_reg.sowing_date"],
-        "block_col": "plot_reg.block"
+        "columns": ["plot_reg-date","plot_reg-block","plot_reg-gp","plot_reg-village","plot_reg-farmer_name","plot_reg-spouse","plot_reg-season","plot_reg-crop_model","plot_reg-main_crop","plot_reg-sowing_date"],
+        "block_col": "plot_reg-block"
     },
     "1.1 NF- Activities": {
         "form_id": "NF- Activities",
